928038f909471292fa34aa9ab5a4443518edbfd3
Kanat Toroev <96743250+kadeezyy@users.noreply.github.com>
2025-01-27 19:15:10 +0300

    Delete core-booking directory

--- core-booking/src/main/kotlin/controller/ApiException.kt
+++
@@ -1,8 +0,0 @@
-package org.example.controller
-
-open class ApiException(
-    val code: String,
-    message: String,
-    val httpStatusCode: Int,
-    cause: Throwable? = null,
-) : Exception(message, cause)
